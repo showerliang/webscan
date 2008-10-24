0966608321412462266825105fa77d6f00af2b4e
seocam <seocam@9d3e173d-1e4d-0410-acad-77672c0258fe>
2008-10-24 16:27:42 +0000

    Changes to make it work using the client

--- server/webscan/server/django/urls.py
+++ server/webscan/server/django/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls.defaults import *
-from webscan.server.django import views
 
-urlpatterns = patterns('',
-    url (r'scanner/$', views.scanner.list, name='list-scanners'),
-    url (r'scanner/(\d+)/$', views.scanner.get, name='get-scanner'),
-    url (r'scanner/(\d+)/scan/$', views.scanner.scan, name='scanner-scan'),
-    url (r'user/$', views.user.list_image_groups, name='user'),
-    url (r'user/(\w+)/$', views.user.list_image_groups, name='list-image-groups'),
-    url (r'user/(\w+)/(\w+)/$', views.user.list_images, name='list-images'),
-    url (r'user/(\w+)/(\w+)/(.*)$', views.user.get_image, name='get-image'),
+urlpatterns = patterns('webscan.server.django.views',
+    url (r'^scanner/$', 'scanner.list',name='list-scanners'),
+    url (r'^scanner/(\d+)/$', 'scanner.get', name='get-scanner'),
+    url (r'^scanner/(\d+)/scan/$', 'scanner.scan', name='scanner-scan'),
+    url (r'^user/$', 'user.info', name='user'),
+    url (r'^user/(?P<username>\w+)/$', 'user.list_image_groups', name='list-image-groups'),
+    url (r'^user/(?P<username>\w+)/(?P<image_group>\w+)/$', 'user.list_images', name='list-images'),
+    url (r'^user/(?P<username>\w+)/(?P<image_group>\w+)/(?P<image_name>.*)$', 'user.get_image', name='get-image'), # Using .* instead of \w+ because of the . in the img name
 )
-
--- server/webscan/server/django/views/user.py
+++ server/webscan/server/django/views/user.py
@@ -1,6 +1,7 @@
 from django.views.static import serve
+from django.views.generic.simple import redirect_to
 from django.http import HttpResponseForbidden, HttpResponseNotFound
-
+from django.core.urlresolvers import reverse
 from webscan.server.django.utils import json, send_file, login_required
 from webscan.lib.core.user import User
 
@@ -9,17 +10,29 @@ from webscan.lib.core.user import User
 def list_image_groups(request, username):
     user = User(username)
     return user.list_image_groups()
+
+@json
+def info(request):
+    if request.user.is_authenticated(): 
+        auth = True
+        username = request.user.username
+    else:
+        auth = False
+        username = None
+    
+    return {'username': username, 'auth': auth}
+    
     
 @login_required
 @json
-def list_images(request, username, group):
+def list_images(request, username, image_group):
     user = User(username)
-    return user.list_images(group)
+    return user.list_images(image_group)
     
 @login_required
-def get_image(request, username, group, image_name):
+def get_image(request, username, image_group, image_name):
     user = User(username)
-    image_path = user.get_image(group, image_name)
+    image_path = user.get_image(image_group, image_name)
     if image_path:
         return send_file(request, image_path)
     else:
