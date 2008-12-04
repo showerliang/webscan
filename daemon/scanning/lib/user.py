061bc65cc08d1abffecb0eeba342e350cb7266ff
seocam <seocam@9d3e173d-1e4d-0410-acad-77672c0258fe>
2008-12-04 20:20:26 +0000

    Adding module the probably will replace the server module

---
+++ daemon/scanning/lib/user.py
@@ -0,0 +1,54 @@
+import os
+from daemon.scanning.lib.document import Document
+
+class User(object):
+    def __init__(self, username=None):
+        if username is None:
+            self.username = os.getenv('USERNAME')
+        else:    
+            self.username = username
+
+        # The following attriutes depends on each os:
+        #   self.home
+        #   self.appdir
+
+        # Posix (Linux, Unix, etc...)
+        if os.name == 'posix':
+            self.home = os.getenv('HOME')
+            self.appdir = self.home + "/.webscan"
+
+        # Windows
+        elif os.name == 'nt':
+            self.home = os.getenv('APPDATA')
+            self.appdir = self.home + "/webscan"
+        
+        # Not supported OS
+        else:
+            raise Exception('OS not supported')
+        
+        self.docdir = self.appdir + "/" + "documents"
+        self.__createappdir()
+        
+        self.documents = {}
+        self.__getdocuments()
+        
+    def __getdocuments(self):
+        docnames = os.listdir(self.docdir)
+        for docname in docnames:
+            self.createdocument(docname)
+
+    def __createappdir(self):
+        """Creates the webscan directory if it don't exists."""   
+        if not os.path.exists(self.appdir):
+            os.mkdir(self.appdir)
+
+        if not os.path.exists(self.docdir):
+            os.mkdir(self.docdir)
+    
+    def createdocument(self, docname):
+        doc = Document(docname, self)
+        self.documents.update({doc.name:doc})
+    
+    def getdocument(self, docname):
+        return self.documents.get(docname)
+    
---
+++ daemon/scanning/views.py
@@ -0,0 +1,69 @@
+import os
+from django.http import HttpResponseNotFound
+from django.core.urlresolvers import reverse
+import pyscanning as scanners
+
+from daemon.scanning.lib.user import User
+from daemon.utils import json, send_file
+
+@json
+def list_scanners(request):
+    return scanners.list()
+
+@json
+def get_scanner_info(request, id):
+    return scanners.get(id).info()
+
+@json
+def scan_page(request, id, docname, pagename):
+    try:
+        scanner = scanners.get(id)
+        image = scanner.scan()
+        user = User()
+        doc = user.getdocument(docname)        
+        if doc is None:
+            user.createdocument(docname)
+            doc = user.getdocument(docname)        
+        doc.addpage(pagename, image)
+    
+        host = 'http://'+request.get_host()
+        host_image = reverse('get-page',args=(user.username, docname, pagename))+".png"
+        return host + host_image
+    except Exception, e:
+        return {'error_msg': str(e)}
+        
+
+def get_page(request, username, docname, pagename):
+    user = User(username)
+    doc = user.getdocument(docname)
+    pagename_without_ext = os.path.splitext(pagename)[0]
+    pagepath = doc.pages[pagename_without_ext].viewpath
+
+    if os.path.exists(pagepath):
+        return send_file(request, pagepath)
+    else:
+        return HttpResponseNotFound()
+
+def get_page_thumb(request, username, docname, pagename):
+    user = User(username)
+    doc = user.getdocument(docname)
+    pagename_without_ext = os.path.splitext(pagename)[0]
+    pagepath = doc.pages[pagename_without_ext].thumbpath
+
+    if os.path.exists(pagepath):
+        return send_file(request, pagepath)
+    else:
+        return HttpResponseNotFound()
+
+@json
+def list_documents(request, username):
+    user = User(username)
+    docs = user.documents
+    return [ docs[key].name for key in docs ]
+
+@json
+def list_doc_pages(request, username, docname):
+    user = User(username)
+    doc = user.getdocument(docname)
+    pages = doc.pages
+    return [ pages[key].name for key in pages ]
